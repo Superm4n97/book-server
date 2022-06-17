24599198290beaba2d62fa1aa7508e15f2481533
rasel <rasel@appscode.com>
2022-06-17 12:02:06 +0600

    Get and Post function added

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Superm4n97/Book-Server
 
 go 1.18
+
+require github.com/go-chi/chi/v5 v5.0.7
--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/Superm4n97/Book-Server/info"
+	"github.com/go-chi/chi/v5"
+	"net/http"
+)
+
+func pong(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("pong"))
+}
 
 func main() {
-	fmt.Println("hello world")
+	r := chi.NewRouter()
+	r.Get("/ping", pong)
+	r.Get("/apis/v1/books", info.GetAllBooks)
+	r.Post("/apis/v1/books", info.AddNewBook)
+
+	fmt.Println("Server Running on port: 8080")
+	http.ListenAndServe(":8080", r)
+
 }
